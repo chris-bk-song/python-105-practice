3cc04ca3e4aa0c629980e8ba1fec4c2c36f63f87
Chris Song <chris.bk.song@gmail.com>
2020-04-06 10:29:57 -0400

    Grocery list

---
+++ prompt_exercise.py
@@ -0,0 +1,30 @@
+# prompt the user for a single grocery item
+#   - append it to the 'groceries' list
+# in an infinite while loop, prompt the user for an item
+#   - append the item to the list
+#   - print() the list after you add the line
+# (To exit out of the loop, press Ctrl C)
+
+
+# Correct way
+
+groceries = []
+
+while True:
+    grocery_item = input('Please enter a grocery item: ')
+
+    groceries.append(grocery_item)
+
+    print(groceries)
+
+# Wrong way
+
+# while True:
+#    groceries = []
+#    grocery_item = input('Please enter a grocery item: ')
+#
+#    groceries.append(grocery_item)
+#
+#    print(groceries)
+
+
