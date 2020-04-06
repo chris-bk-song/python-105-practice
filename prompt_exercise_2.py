ace5047faac9c679983f330baf223cd00ff2e7b5
Chris Song <chris.bk.song@gmail.com>
2020-04-06 10:38:10 -0400

    How to change item in my grocery list

---
+++ prompt_exercise_2.py
@@ -0,0 +1,3 @@
+groceries = ['Milk', 'Bread', 'Beer'] #Defined what groceries are consist of
+groceries[0] = 'Oyster' # Change the 1st (0) grocery item to Oyster
+print(groceries) # display new grocery list
