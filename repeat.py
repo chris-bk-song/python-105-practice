1f1fb64348ac98e40e6662a23e9758de05def99c
Chris Song <chris.bk.song@gmail.com>
2020-04-06 10:01:34 -0400

    A function to repeat a given string for given numbers of times

---
+++ repeat.py
@@ -0,0 +1,16 @@
+# 04-06-20 Monday morning start exercise 
+# Write a function named "repeat"
+# It should accept 2 arguments:
+# a number and a string
+# It should print out the string however many times you specify
+# repeat(3 'good morning')
+# good morning!
+# good morning!
+# good morning!
+
+def repeat (num, string):
+    return num * string
+
+printout = repeat(3, 'good morning!' '\n')
+
+print(printout)
