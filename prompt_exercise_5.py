39fe2d17bd2294b5cc8e54b2a83468ef12891226
Chris Song <chris.bk.song@gmail.com>
2020-04-06 12:16:56 -0400

    Slicing

---
+++ prompt_exercise_5.py
@@ -0,0 +1,6 @@
+# Access and replace multiple items in a list at a time
+
+groceries = ['Milk', 'Bread', 'Beer', 'Cheese', 'Churos']
+print(groceries[0:2]) # slicing starting from index 0 and ends at index 2
+print(groceries[1:2]) # slicing starting from index 1 and ends at index 2
+print(groceries[:]) # omit start and end number and just put colon = list all
