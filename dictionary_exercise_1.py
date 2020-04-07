8df872162bcbded981dad44188bd0b216ca28397
Chris Song <chris.bk.song@gmail.com>
2020-04-07 11:39:14 -0400

    Dictionary exercise 1

---
+++ dictionary_exercise_1.py
@@ -0,0 +1,12 @@
+# Create a dictionary of friend
+
+friend = {
+    'name': 'Elliot Alderson',
+    'cell': 8565309,
+    'snack': ['popcorn', 'reeces pieces']
+}
+
+# Update dictionary with information about cat
+friend['cat'] = 'Mr Robot'
+print(friend)
+print(friend['cat'])
