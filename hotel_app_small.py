de2b32a1952689d1fa9c84c1861eaf9663fd8b08
Chris Song <chris.bk.song@gmail.com>
2020-04-08 10:25:39 -0400

    Adds main menu option for hotel app

--- hotel_app_small.py
+++ hotel_app_small.py
@@ -21,65 +21,65 @@ hotel = {
     '105': {},
 }
 
-# Write functions to:
-# is_vacant(which_hotel, '101')
-# -check if a room is occupied
+main_menu = '''
 
-room_number = hotel.keys()
+1. Print hotel room status
+2. Check in customer
+3. Check out customer
+4. Quit
 
-def is_vacant(room_number):
-    for room_number in hotel.keys():
-        if hotel[room_number] == {}:
-            print(f'Room {room_number} is vacant.\n')
-        else:
-            print(f'Room {room_number} is occupied by {hotel[room_number]}.\n')
+'''
 
-print('\n[The current list of the occupied and vacant rooms is shown below]\n')
-print(f'{is_vacant(room_number)}\n')
+while True:
+    menu_choice = int(input(main_menu))
 
-# check_in('101', guest_dictionary)
-# -assign a person to a room
+    room_number = hotel.keys()
+    
+    if menu_choice == 1:
+        def is_vacant(room_number):
+            for room_number in hotel.keys():
+                if hotel[room_number] == {}:
+                    print(f'Room {room_number} is vacant.')
+                else:
+                    print(f'Room {room_number} is occupied by {hotel[room_number]}.')
 
-assign_room = hotel.keys()
+        print(f'{is_vacant(room_number)}\n')
+        
+    elif menu_choice == 2:
 
-def check_in(assign_room):
-    for assign_room in hotel.keys():
-        if hotel[assign_room] == {}:
-            print(f'Room {assign_room} is available.\n')
-            yes_or_no = input('Do you want to take this room? [Y/N]\n')
-            if yes_or_no == 'Y':
-                new_name = input('What is the name?\n')
-                new_phone = input('What the phone number?\n')
-                hotel[assign_room]['guest'] = {'name': new_name, 'phone': new_phone}
-            elif yes_or_no == 'N':
-                print('[This room is unoccupied.]\n')
-            else:
-                print('[Please type either Y or N.]\n')
-        else:
-            print(f'Room {assign_room} is already occupied.\n')
+        def check_in(room_number):
+            for room_number in hotel.keys():
+                if hotel[room_number] == {}:
+                    print(f'Room {room_number} is available.')
+                    yes_or_no = input('Do you want to take this room? [Y/N]\n')
+                    if yes_or_no == 'Y':
+                        new_name = input('What is the name?\n')
+                        new_phone = input('What the phone number?\n')
+                        hotel[room_number]['guest'] = {'name': new_name, 'phone': new_phone}
+                    elif yes_or_no == 'N':
+                        print('[This room is unoccupied.]\n')
+                    else:
+                        print('[Please type either Y or N.]')
+                else:
+                    print(f'Room {room_number} is already occupied.')
 
-print(f'{check_in(assign_room)}\n')
-print('[The updated list of occupied and vacant rooms is shown below after check-in]\n')
-print(f'{is_vacant(room_number)}\n')
+        print(f'{check_in(room_number)}\n')
 
+    elif menu_choice == 3:
 
+        def check_out(room_number):
+            for room_number in hotel.keys():
+                if hotel[room_number] != {}:
+                    remove = input(f'Do you want check out of room {room_number}? [Y/N]\n')
+                    if remove == 'Y':
+                        hotel[room_number] = {}
+                        print(f'[Room {room_number} has been checked out.]\n')
+                    elif remove == 'N':
+                        print('[Enjoy your stay.]\n')
 
+        print(f'{check_out(room_number)}\n')
 
-# check_out('101')
-# -return the person dictionary in that room 
-
-empty_room = hotel.keys()
-
-def check_out(empty_room):
-    for empty_room in hotel.keys():
-        if hotel[empty_room] != {}:
-            remove = input(f'Do you want check out of room {empty_room}? [Y/N]\n')
-            if remove == 'Y':
-                hotel[empty_room] = {}
-                print(f'[Room {empty_room} has been checked out.]\n')
-            elif remove == 'N':
-                print('[Enjoy your stay.]\n')
-
-print(f'{check_out(empty_room)}\n')
-print('[The updated list of occupied and vacant rooms is shown below after check-out]\n')
-print(f'{is_vacant(room_number)}\n')
+    elif menu_choice == 4:
+        break
+        
+print('Thank you for using the hotel app!')
